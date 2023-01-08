3b0efd6921c11021b5a1bd47af85e19e9605fd13
RushiGithub03 <rushigithub03@gmail.com>
2023-01-08 13:10:29 +0530

    added linkedlist

---
+++ src/com/collection/demo/LinkedListDemo.java
@@ -0,0 +1,16 @@
+package com.collection.demo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+
+	
+		List<Integer>lls= new LinkedList<Integer>();
+		lls.add(2);
+		System.out.println(lls);
+	}
+
+}
